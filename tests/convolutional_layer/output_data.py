033b5dca04afc3e6d6ecfe6859aae055ade61632
Nick Park <nick.park@student.manchester.ac.uk>
2019-02-19 18:42:41 +0000

    read weights from file in software model

--- tests/convolutional_layer/output_data.py
+++ tests/convolutional_layer/output_data.py
@@ -9,8 +9,11 @@ f_size = 5
 stride = 4
 in_width = 64
 in_height = 32
+in_channels = 3
 out_width = (in_width - (f_size-1)) / stride
 out_height = (in_height - (f_size-1)) / stride
+out_channels = 5
+
 
 def int2hex(number):
   bits = 16
@@ -25,6 +28,13 @@ def int2str(val, bits=16):
   return str(int2hex(val)).zfill(bits/4)
 
 
+def hex2int(hexstr,bits=8):
+  value = int(hexstr,16)
+  if value & (1 << (bits-1)):
+    value -= 1 << bits
+  return value
+
+
 def printArray(array, file):
   with open(file, 'w') as fh:
     for y in range(array.shape[1]):
@@ -36,6 +46,10 @@ def printArray(array, file):
       print(file=fh)
 
 
+def flip_array(array):
+  return np.flipud(np.fliplr(array))
+
+
 def convolve(array, filter):
   filter2 = np.flipud(np.fliplr(filter))
   res = np.zeros((out_height, out_width)).astype(int)
@@ -45,18 +59,31 @@ def convolve(array, filter):
   return res
 
 
-np.random.seed(0)
-input = (256 * np.random.random((3, in_height, in_width))).astype(int) - 128
+def read_filter_weights():
+  filters = np.empty((out_channels, in_channels, f_size, f_size)).astype(int)
+  for out_channel in range(0, out_channels):
+    for in_channel in range(0, in_channels):
+      with open("../../data/alexnet/layer_0/filter_{}_{}.hex".format(out_channel, in_channel)) as fh:
+        weights = fh.read().splitlines()
+        weights = map(lambda x: hex2int(x), weights)
+        weights = np.asarray(weights).reshape((f_size, f_size))
+        filters[out_channel,in_channel] = flip_array(weights)
+  return filters
 
-result = np.zeros((5, out_height, out_width)).astype(int)
+
+filters = read_filter_weights()
+
+np.random.seed(0)
+input = (256 * np.random.random((in_channels, in_height, in_width))).astype(int) - 128
+
+result = np.zeros((out_channels, out_height, out_width)).astype(int)
 
 np.random.seed(1)
 filter = (64 * np.random.random((f_size, f_size))).astype(int) - 32
 
 for out_channel in range(0, result.shape[0]):
   for in_channel in range(0, input.shape[0]):
-    # result[out_channel] = result[out_channel] + sp.convolve2d(input[in_channel], filter, mode='valid', boundary='wrap')
-    result[out_channel] = result[out_channel] + convolve(input[in_channel], filter)
+    result[out_channel] = result[out_channel] + convolve(input[in_channel], filters[out_channel, in_channel])
 
 
 printArray(result, 'output_data.hex')
